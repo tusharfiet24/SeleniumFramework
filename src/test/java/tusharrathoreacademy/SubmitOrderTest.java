1259ceae9619eca17bce825578a86ef7d50b33fe
tusharfiet24 <ertusharfiet24@gmail.com>
2024-09-05 00:12:50 +0530

    Refactor the code

--- src/test/java/tusharrathoreacademy/SubmitOrderTest.java
+++ src/test/java/tusharrathoreacademy/SubmitOrderTest.java
@@ -14,38 +14,25 @@ import tusharrathoreacademy.pageobjects.ProductCatalogue;
 public class SubmitOrderTest {
 
 	public static void main(String[] args) {
-		String productName = "ZARA COAT 3";
 
+		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-
-		// Implicit Wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.manage().window().maximize();
-
-		// Login Page
 		LandingPage landingPage = new LandingPage(driver);
 		landingPage.goTo();
-
-		// Catalogue Page
 		ProductCatalogue productCatalogue = landingPage.loginApplication("Arjun@gmail.com", "Arjun@1998@");
 		productCatalogue.addProductToCart(productName);
 
-		// Cart Page
 		CartPage cartPage = productCatalogue.goToCartPage();
 		Assert.assertTrue(cartPage.verifyProductDisplay(productName));
-
-		// Checkout Page
 		CheckoutPage checkoutPage = cartPage.goToCheckOut();
 		checkoutPage.enterDetails("Ind");
 		checkoutPage.selectCountry("India");
-
-		// Confirmation Page
 		ConfirmationPage confirmationPage = checkoutPage.submitOrder();
 		String confirmText = confirmationPage.getConfirmText();
 		Assert.assertTrue(confirmText.trim().equalsIgnoreCase("Thankyou for the order."));
-
-		System.out.println(confirmationPage.getOrderId());
 		driver.quit();
 	}
 }
