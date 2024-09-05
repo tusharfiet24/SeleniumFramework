41a2e63e713975a82a8a2af7d1ee4928b965a5cf
tusharfiet24 <ertusharfiet24@gmail.com>
2024-09-05 20:27:22 +0530

    implementing Parameterization into tests with TestNG Data provider

--- src/test/java/tusharrathoreacademy/tests/SubmitOrderTest.java
+++ src/test/java/tusharrathoreacademy/tests/SubmitOrderTest.java
@@ -2,6 +2,7 @@ package tusharrathoreacademy.tests;
 
 import java.io.IOException;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import tusharrathoreacademy.TestComponents.BaseTest;
 import tusharrathoreacademy.pageobjects.CartPage;
@@ -12,10 +13,10 @@ import tusharrathoreacademy.pageobjects.ProductCatalogue;
 
 public class SubmitOrderTest extends BaseTest {
 	String productName = "ZARA COAT 3";
-	
-	@Test
-	public void submitOrder() throws IOException {
-		ProductCatalogue productCatalogue = landingPage.loginApplication("Arjun@gmail.com", "Arjun@1998@");
+
+	@Test(dataProvider = "getData", groups = { "Purchase" })
+	public void submitOrder(String email, String password, String productName) throws IOException {
+		ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
 		productCatalogue.addProductToCart(productName);
 		CartPage cartPage = productCatalogue.goToCartPage();
 		Assert.assertTrue(cartPage.verifyProductDisplay(productName));
@@ -33,5 +34,11 @@ public class SubmitOrderTest extends BaseTest {
 		ProductCatalogue productCatalogue = landingPage.loginApplication("Arjun@gmail.com", "Arjun@1998@");
 		OrderPage orderPage = productCatalogue.goToOrdersPage();
 		Assert.assertTrue(orderPage.verifyOrderDisplay(productName));
+	}
+
+	@DataProvider
+	public Object[][] getData() {
+		return new Object[][] { { "Arjun@gmail.com", "Arjun@1998@", "ZARA COAT 3" },
+				{ "Govind@gmail.com", "Govind@1", "ADIDAS ORIGINAL" } };
 	}
 }
